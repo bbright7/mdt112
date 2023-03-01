5e1464871ae656f802436a042ff6549ed2f8c8bf
bbright7 <123539698+bbright7@users.noreply.github.com>
2023-03-01 15:59:46 +0700

    1.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace bbright{}
+    class program{
+        static void Main(string[] args){
+            int sum = 0;
+            for(int = 0; i < 7; i++) {
+                for(int j = 0; j < 3; ++j){
+                    sum = sum +1;
+
+                }
+                Console.WriteLine("sum is: {0}" , sum);
+            }
+            }
+            
+        }
